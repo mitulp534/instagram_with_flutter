d342aa1881afb804ed66df47b1a081b9f88c0e46
mitulp534 <mitulp534@gmail.com>
2018-10-25 22:07:16 +0530

    This is the basic UI of Instagram which i made using flutter

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_with_flutter/insta_home.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "Instagram",
+      debugShowCheckedModeBanner: false,
+      theme: new ThemeData(
+        primaryColor: Colors.black,
+        primaryIconTheme: IconThemeData(
+          color: Colors.black,
+        ),
+        primaryTextTheme: TextTheme(
+          title: TextStyle(color: Colors.black)
+        ),
+        textTheme: TextTheme(title: TextStyle(color: Colors.black,fontFamily: "Aveny")),
+      ),
+      home: new InstaHome(),
+    );
+  }
+}
